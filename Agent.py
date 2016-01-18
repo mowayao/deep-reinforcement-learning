8a74c371e302fe35bc9197aee7f09c61221a8acf
mowayao <493745925@qq.com>
2016-01-18 20:06:52 +0800

    agent

--- Agent.py
+++ Agent.py
@@ -4,6 +4,7 @@ game agent
 '''
 import logging
 import numpy as np
+import time
 class Agent:
 	'''
 
@@ -29,38 +30,34 @@ class Agent:
 
 		#TODO:update self.epsilon
 
-		if(self.memory_pool.curSz > self.replay_start_size):
-			pass
-		else:
+		#if(self.memory_pool.curSz > self.replay_start_size):
+			#None
+	def train_my_model(self):
+		phi,action,reward,phis,terminal = self.memory_pool.stochasticSample(self.batch_size)
+		self.ddqn.train(phi,action,reward,phis,terminal)
 
-			pass
-
-
-	def __train_my_model(self):
-	'''
-	train my network and return loss
-	'''	
-		phi,action,reward,phis,terminal = self.memory_pool(self.batch_size)
-		return self.train(phi,action,reward,phis,terminal)
-		
 
 	def greedy_action(self,phi):
-	'''
-	choose the epsilon-greedy action
-	'''
-		if rng.random() < self.epsilon:
-			action = self.ddqn.choose_action(phi)
+
+		if self.rng.rand() < self.epsilon:
+			action = self.valid_actions[self.rng.randint(0,self.num_valid_actions)]
 		else:
-			action = rng.randint(0,self.num_valid_actions)
-		
+			action = self.ddqn.choose_action(phi)
 		return action
 	def start_episode(self,phi):
 		self.step_cnt = 0
+		self.batch_cnt = 0
+		self.episode_reward = 0
 		self.loss = []
-		pass
+		self.start_time = time.time()
+		action = self.valid_actions[self.rng.randint(self.num_valid_actions)]
+		self.last_action = action
+		self.last_phi = phi
+		return action
 			
 	def finish_epoch(self,epoch):
 		pass
 		#TODO: save model weights
-
+	def finish_episode(self,reward,terminal):
+		pass
 
